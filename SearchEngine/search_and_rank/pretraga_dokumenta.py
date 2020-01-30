2e3930c351e48b69d4b573e84b9df4c9daad1c5f
Sladjana <sladjasavkovic333@gmail.com>
2020-01-30 19:43:27 +0100

    #pretraga_dokumenata Funkcija koja vraca skup HTML stranica za upit

---
+++ SearchEngine/search_and_rank/pretraga_dokumenta.py
@@ -0,0 +1,64 @@
+import os
+from main.parser import Parser
+
+
+def pretraga_dokumenta(root,path,words,operator):
+    #Rezultat pretrage je skup stranica koje zadovoljavaju upit
+    result = set()
+    p = Parser()
+    #path_set sadrzi sve putanje od unesenog direktorijuma zajedno sa njegovim poddirektorijumima
+    path_set = set()
+    for cur, dirs, files in os.walk(path):
+        for f in files:
+            if ".html" in f:
+                path_set.add(os.path.join(cur,f))
+
+    op_set = set()
+    if operator == "AND":
+        a1, b1, c1 = root.find_word(words[0])
+        a2, b2, c2 = root.find_word(words[1])
+        if len(c1) is 0 or len(c2) is 0:
+            return None
+        else:
+            op_set = c1.intersection(c2)
+    if operator == "OR":
+        a1,b1,c1 = root.find_word(words[0])
+        a2, b2, c2 = root.find_word(words[1])
+        if len(c1) is 0 and len(c2) is 0:
+            return None
+        elif len(c1) is 0:
+            op_set = c2
+        elif len(c2) is 0:
+            op_set = c1
+        else:
+            op_set = c1.union(c2)
+    if operator == "NOT":
+        a1, b1, c1 = root.find_word(words[0])
+        a2, b2, c2 = root.find_word(words[1])
+        if (len(c1) is 0 and len(c2) is not 0) or (len(c1) is 0 and len(c2) is 0):
+            return None
+        elif len(c1) is not 0 and len(c2) is 0:
+            op_set = c1
+        else:
+            op_set = c1.difference(c2)
+    else:
+        flag = False
+        for i in words:
+            a1, b1, c1 = root.find_word(i)
+            if len(c1) is not 0 and flag is False:
+                rez = c1
+                flag = True
+            if len(c1) is not 0 and flag is True:
+                rez.union(c1)
+
+        if flag is True:
+            op_set = rez
+        else:
+            op_set = None
+
+    if (path_set is None) or (op_set is None):
+        result = None
+    else:
+        result = op_set.intersection(path_set)
+
+    return result
