c4732dfba6a3aa4eeb69a490e1516fc8c07223e6
Sladjana <sladjasavkovic333@gmail.com>
2020-02-03 11:26:22 +0100

    #parsiranje_skupa_HTML_dokumenata Poziv funkcija za izgradnju stabla i grafa u main-u

--- SearchEngine/main/def_main.py
+++ SearchEngine/main/def_main.py
@@ -2,12 +2,11 @@ from main.unos_upita import *
 from graph.graph_class import *
 from search_and_rank.pretraga_dokumenta import *
 from search_and_rank.rangirana_pretraga import *
-from trie_stablo import stablo
-
+from main.parsiranje_HTML_doc import *
 
 def main():
     path = 'C:\\Users\\pc\Desktop\\Stabla\\python-2.7.7-docs-html'
-    root = stablo.make_tree(path)
+    root = make_tree(path)
     g,graph = make_graph(path)
 
     putanja = input("Unesite putanju do direktorijuma: ")
