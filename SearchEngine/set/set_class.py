3f6e8579c1dcd4aba32aff7dde68ecae5d6afcfa
Dragana Carapic <dragana.carapic98@uns.ac.rs>
2020-02-04 20:01:29 +0100

    #set Izmjena klase za implementaciju set-a

--- SearchEngine/set/set_class.py
+++ SearchEngine/set/set_class.py
@@ -2,49 +2,66 @@
 class Set(object):
 
     def __init__(self):
-        self.dict = {}
-    def add(self, e):
-        self.dict[e ] =e
+        self.table = [[] for i in range(50)]
+        self.n = 0
 
-    def __len__(self):
-        return len(self.dict)
+    def __str__(self):
+        a=""
+        for e in self.table:
+            a += str(e) + "\n"
+        return a
 
-    def __iter__(self):
-        return iter(self.dict.copy())
 
-    def __contains__(self, e):
-        if self.dict.get(e) is not None:
-            return True
+    def add(self, key):
+        hash_key = hash(key) % len(self.table)
+        key_exists = False
+        bucket = self.table[hash_key]
+        for i, k in enumerate(bucket):
+            if key == k:
+                key_exists = True
+                break
+        if not key_exists:
+            bucket.append(key)
+            self.n += 1
 
-        else:
-            return False
-    def remove(self ,e):
-        del self.dict[e]
+    def search(self, key):
+        hash_key = hash(key) % len(self.table)
+        bucket = self.table[hash_key]
+        for k in bucket:
+            if key == k:
+                return k
 
     def __or__(self, other):
         result = Set()
-        for e in self:
-            result.add(e)
-        for e in other:
-            result.add(e)
+        for e in self.table:
+            for k in e:
+                result.add(k)
+        for e in other.table:
+            for k in e:
+                result.add(k)
         return result
 
     def __and__(self, other):
         result = Set()
-        for e in other:
-            if e in self.dict:
-                result.add(e)
+        for e in other.table:
+            for k in e:
+                if self.search(k) is not None:
+                    result.add(k)
         return result
 
     def __sub__(self, other):
         result = Set()
-        for e in self:
-            if e not in other:
-                result.add(e)
+        for e in self.table:
+            for k in e:
+                if other.search(k) is None:
+                    result.add(k)
         return result
 
-    def __str__(self):
-        a=""
-        for e in self.dict:
-            a += str(e) + "\n"
-        return a
+    def __len__(self):
+        return self.n
+
+    def __iter__(self):
+        for bucket in self.table:
+            if bucket is not None:
+                for key in bucket:
+                    yield key
