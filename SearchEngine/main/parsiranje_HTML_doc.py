795e09f9baad66af911e6ee5dc9aca9e420453c1
Sladjana <sladjasavkovic333@gmail.com>
2020-02-11 21:43:33 +0100

    #parsiranje_skupa_HTML_dokumenata Objedinjavanje dvije metode u jednu zbog brzine

--- SearchEngine/main/parsiranje_HTML_doc.py
+++ SearchEngine/main/parsiranje_HTML_doc.py
@@ -3,28 +3,18 @@ from graph.graph_class import Graph
 from main.parser import Parser
 from trie_stablo.stablo import TrieNode
 
-def make_graph(dirname):
+def make_tree_and_graph(dirname):
     p = Parser()
-
-    g={}
-    for r,d,f in os.walk(dirname):
-        for file in f:
-            if ".html" in file:
-                link,word=p.parse(os.path.join(r,file))
-                g[os.path.join(r,file)] = link
-
-    graph=Graph(g)
-    return g,graph
-
-def make_tree(dirname):
-    p = Parser()
+    g = {}
     root = TrieNode('*')
     for cur, dirs, files in os.walk(dirname):
         for f in files:
             if ".html" in f:
                 link, words = p.parse(os.path.join(cur,f))
+                g[os.path.join(cur, f)] = link
                 i = 0
                 while i < len(words):
                     root.add(words[i], os.path.join(cur,f))
                     i += 1
-    return root
+    graph = Graph(g)
+    return root,g,graph
