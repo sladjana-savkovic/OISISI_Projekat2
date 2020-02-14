8640bf2b2f9763fe2c69be16a959df969e2ecabe
Sladjana <sladjasavkovic333@gmail.com>
2020-02-14 18:13:24 +0100

    #parsiranje_skupa_HTML_dokumenata Nova metoda koja se rekurzivno poziva radi ubrzanja

--- SearchEngine/main/parsiranje_HTML_doc.py
+++ SearchEngine/main/parsiranje_HTML_doc.py
@@ -1,20 +1,32 @@
 import os
+from os.path import isfile, isdir
 from graph.graph_class import Graph
 from main.parser import Parser
 from trie_stablo.stablo import TrieNode
 
-def make_tree_and_graph(dirname):
+
+def make_tree_and_graph(f):
     p = Parser()
     g = {}
     root = TrieNode('*')
-    for cur, dirs, files in os.walk(dirname):
-        for f in files:
-            if ".html" in f:
-                link, words = p.parse(os.path.join(cur,f))
-                g[os.path.join(cur, f)] = link
-                i = 0
-                while i < len(words):
-                    root.add(words[i], os.path.join(cur,f))
-                    i += 1
+
+    recursive_walk(f, p, g, root)
     graph = Graph(g)
     return root,g,graph
+
+def recursive_walk(f,p,g,root):
+
+    if isfile(f) is not False and ".html" in f: #f je html fajl, bazni slucaj
+        link, words = p.parse(f)
+        g[f] = link
+        i = 0
+        while i < len(words):
+            root.add(words[i], f)
+            i += 1
+
+    elif isdir(f) is not False:  #f je direktorijum
+        for dir in os.listdir(f):
+            recursive_walk(os.path.join(f,dir),p,g,root)
+
+
+
