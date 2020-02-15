9c75671b25b21b70a4d2f9235ba307946ea5af08
Sladjana <sladjasavkovic333@gmail.com>
2020-02-15 22:15:00 +0100

    #pretraga_dokumenata Izmjena radi ponavljanja dijelova koda

--- SearchEngine/search_and_rank/pretraga_dokumenta.py
+++ SearchEngine/search_and_rank/pretraga_dokumenta.py
@@ -1,65 +1,52 @@
-import os
-from main.parser import Parser
 from set.set_class import *
 
-
-def pretraga_dokumenta(root,path,words,operator):
+def pretraga_dokumenta(root,words,operator):
     #Rezultat pretrage je skup stranica koje zadovoljavaju upit
     result = Set()
-    p = Parser()
-    #path_set sadrzi sve putanje od unesenog direktorijuma zajedno sa njegovim poddirektorijumima
-    path_set = Set()
-    for cur, dirs, files in os.walk(path):
-        for f in files:
-            if ".html" in f:
-                path_set.add(os.path.join(cur,f),os.path.join(cur,f))
 
-    op_set = Set()
-    if operator == "AND":
+    #Upit se sastoji iz dvije rijeci i operatora
+    if operator in ['AND','OR','NOT']:
+        #Odredjivanje skupa HTML stranica koje sadrze pojedinacne rijeci upita
         c1 = root.find_word(words[0])
         c2 = root.find_word(words[1])
-        if len(c1) is 0 or len(c2) is 0:
-            return None
-        else:
-            op_set = c1 & c2
-    if operator == "OR":
-        c1 = root.find_word(words[0])
-        c2 = root.find_word(words[1])
-        if len(c1) is 0 and len(c2) is 0:
-            return None
-        elif len(c1) is 0:
-            op_set = c2
-        elif len(c2) is 0:
-            op_set = c1
-        else:
-            op_set = c1 | c2
-    if operator == "NOT":
-        c1 = root.find_word(words[0])
-        c2 = root.find_word(words[1])
-        if (len(c1) is 0 and len(c2) is not 0) or (len(c1) is 0 and len(c2) is 0):
-            return None
-        elif len(c1) is not 0 and len(c2) is 0:
-            op_set = c1
-        else:
-            op_set = c1 - c2
+        if operator == "AND":
+            if len(c1) is 0 or len(c2) is 0:
+                return None
+            else:
+                result = c1 & c2
+        elif operator == "OR":
+            if len(c1) is 0 and len(c2) is 0:
+                return None
+            elif len(c1) is 0:
+                result = c2
+            elif len(c2) is 0:
+                result = c1
+            else:
+                result = c1 | c2
+        elif operator == "NOT":
+            if (len(c1) is 0 and len(c2) is not 0) or (len(c1) is 0 and len(c2) is 0):
+                return None
+            elif len(c1) is not 0 and len(c2) is 0:
+                result = c1
+            else:
+                result = c1 - c2
+    #Upit se sastoji iz vise rijeci
     else:
+        # Odredjivanje skupa HTML stranica koje sadrze pojedinacne rijeci upita
         flag = False
-        for i in words:
-            c1 = root.find_word(i)
+        for word in words:
+            c1 = root.find_word(word)
+            #Naisli smo na prvu rijec koja se nalazi u stablu
             if len(c1) is not 0 and flag is False:
                 rez = c1
                 flag = True
+            #Naisli smo na jos neku rijec koja se nalazi u stablu
             if len(c1) is not 0 and flag is True:
                 rez = rez | c1
 
         if flag is True:
-            op_set = rez
+            result = rez
         else:
-            op_set = None
-
-    if (path_set is None) or (op_set is None):
-        result = None
-    else:
-        result = op_set & path_set
+            result = None
 
     return result
