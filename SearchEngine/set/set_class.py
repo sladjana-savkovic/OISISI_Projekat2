d3c85f5d7c47a59b8919a3d0139189488d937f04
Dragana Carapic <dragana.carapic98@uns.ac.rs>
2020-02-22 15:13:08 +0100

    #set Dodavanje novih metoda u klasu Set

--- SearchEngine/set/set_class.py
+++ SearchEngine/set/set_class.py
@@ -10,6 +10,9 @@ class Set(object):
     def ret_key(self):
         return self.dict.keys()
 
+    def ret_all_val(self):
+        return  self.dict.values()
+
     def ret_val(self, key):
         if self.dict.get(key) is not None:
             return self.dict[key]
@@ -36,23 +39,32 @@ class Set(object):
     def __or__(self, other):
         result = Set()
         for e in self.ret_key():
-            result.add(e,e)
+            result.add(e,self.ret_val(e))
         for e in other.ret_key():
-            result.add(e,e)
+            if e in result.ret_key():
+                result.add(e, 3*(result.ret_val(e) + other.ret_val(e)))
+            else:
+                result.add(e,other.ret_val(e))
         return result
 
     def __and__(self, other):
         result = Set()
-        for e in other.ret_key():
-            if e in self.ret_key():
-                result.add(e,e)
+        if len(other) < len(self):
+            for e in other.ret_key():
+                if e in self.ret_key():
+                    result.add(e,other.ret_val(e) + self.ret_val(e))
+        else:
+            for e in self.ret_key():
+                if e in other.ret_key():
+                    result.add(e,other.ret_val(e) + self.ret_val(e))
+
         return result
 
     def __sub__(self, other):
         result = Set()
         for e in self.ret_key():
             if e not in other.ret_key():
-                result.add(e,e)
+                result.add(e,self.ret_val(e))
         return result
 
     def __str__(self):
@@ -60,3 +72,11 @@ class Set(object):
         for e in self.dict:
             a += str(e)+ " " + str(self.dict[e]) + "\n"
         return a
+
+
+if __name__ == "__main__":
+    d=Set()
+    d.add(11,"a")
+    d.add(22,"b")
+    d.add(33,"c")
+    print(d.ret_key())
