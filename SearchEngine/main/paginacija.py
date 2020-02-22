eae2942f850158182f1e09be52518090951fac9b
Dragana Carapic <dragana.carapic98@uns.ac.rs>
2020-02-22 17:19:16 +0100

    #paginacija_rezultata Izmjena prikaza rezultata

--- SearchEngine/main/paginacija.py
+++ SearchEngine/main/paginacija.py
@@ -3,13 +3,16 @@ def paginacija(list, n):
     i = 0
     while True:
         ispis(list,i,i+n)
-        if len(list) == 1:
+        if len(list) <= 1 or len(list) == n:
             print("Kraj paginacije(0)")
         elif i == 0:
+            print("Broj preostalih stranica: " + str(len(list) - n))
             print("Sledeca(>)   Promjena broja stranica(*)   Kraj paginacije(0)")
         elif i >= len(list)-n:
+            print("Broj preostalih stranica: 0")
             print("Prethodna(<)   Promjena broja stranica(*)   Kraj paginacije(0)")
         else:
+            print("Broj preostalih stranica: " + str(len(list) - i - n))
             print("Prethodna(<)   Sledeca(>)   Promjena broja stranica(*)   Kraj paginacije(0)")
         a = input()
         if a == '<':  # korisnik unio <
