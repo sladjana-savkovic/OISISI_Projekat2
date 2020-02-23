a8056c30dbe140d009cf60af89bf95aba32babfe
Sladjana <sladjasavkovic333@gmail.com>
2020-02-23 20:04:29 +0100

    #unos_upita Izmjena zbog insensitive pretrage

--- SearchEngine/main/unos_upita.py
+++ SearchEngine/main/unos_upita.py
@@ -2,28 +2,28 @@
 def parsiraj_upit(upit):
     words = []
     upit = upit.strip() #uklanjanje razmaka sa pocetka i kraja upita
+    upit = upit.lower()
 
     if " " not in upit:
-        if upit.upper() in ['AND','OR','NOT']:
+        if upit in ['and','or','not']:
             return None,None
         words.append(upit)
         return None, words
     else:
         text = upit.split() #split po whitespace karakterima
-        #Primjer upita: NOT Python
-        if (text[0].upper() == "NOT") and (len(text) is 2) and (text[1].upper() not in ['AND','OR','NOT']):
+        #Primjer upita: not Python
+        if (text[0] == "not") and (len(text) is 2) and (text[1] not in ['and','or','not']):
             words.append(text[1])
             return text[0],words
-        #Primjer upita: Python AND Java
-        elif (text[1].upper() in ['AND','OR','NOT']) and (len(text) is 3) \
-                and (text[0].upper() not in ['AND','OR','NOT']) and (text[2].upper() not in ['AND','OR','NOT']):
+        #Primjer upita: Python and Java
+        elif (text[1] in ['and','or','not']) and (len(text) is 3) \
+                and (text[0] not in ['and','or','not']) and (text[2] not in ['and','or','not']):
             words.append(text[0])
             words.append(text[2])
             return text[1],words
         #Primjer upita: python java
-        elif ("AND" not in text) and ("OR" not in text) and ("NOT" not in text) \
-                and ("and" not in text) and ("or" not in text) and ("not" not in text):
+        elif ("and" not in text) and ("or" not in text) and ("not" not in text):
             return None,text
-        #Pogresan upit: and AND Java
+        #Pogresan upit: and and Java
         else:
             return None,None
