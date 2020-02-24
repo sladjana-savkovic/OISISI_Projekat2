4d2c74c7082872c5e3661ae23e0cfcd9b157f1fd
Dragana Carapic <dragana.carapic98@uns.ac.rs>
2020-02-24 20:06:45 +0100

    #graph Izmjena metode za rangiranu pretragu

--- SearchEngine/search_and_rank/rangirana_pretraga.py
+++ SearchEngine/search_and_rank/rangirana_pretraga.py
@@ -12,19 +12,20 @@ def scale_number(unscaled, to_min, to_max, from_min, from_max):
 def scale_list(l, to_min, to_max):
     return [scale_number(i, to_min, to_max, min(l), max(l)) for i in l]
 
-def rang_pretraga(g, graph, words, result, operator,root):
+def rang_pretraga(g, words, result, operator,root):
     list=[]
 
     r1,r2,r3=[],[],[]
-    if operator in ["AND", "and", "NOT", "not"]:
+    if operator in ["and", "not"]:
         r1 = result.ret_all_val()
+
     for page in result:
-        links = links_for_rank(g,graph,page)
+        links = links_for_rank(g,page)
         r2.append(len(links))
 
-        if operator not in ["AND", "and", "NOT", "not"]:
+        if operator not in ["and", "not"]:
             broj_rijeci, broj_nenultih = root.find_word_document(words, page)
-            r1.append(sum(broj_rijeci) / len(broj_rijeci) * broj_nenultih / len(broj_rijeci))
+            r1.append(sum(broj_rijeci) * broj_nenultih / len(broj_rijeci))
 
         p = result & links
         if p is not None:
